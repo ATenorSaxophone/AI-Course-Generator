9144620c5e47ba98005c5a4eb8199930bca72df7
ATenorSaxophone <noah.benner@gmail.com>
2026-05-26 09:19:53 -0400

    Created Agent with tools and DeepSeek model.

--- agent.py
+++ agent.py
@@ -36,3 +36,14 @@ def create_canvas_quiz():
 @tool
 def create_canvas_assignment():
     return 0
+
+#create agent with tools and deepseek model
+agent = create_deep_agent(
+    model=deepseek_model,
+    tools=[
+        create_canvas_module,
+        create_canvas_page,
+        create_canvas_quiz,
+        create_canvas_assignment
+    ],
+)
