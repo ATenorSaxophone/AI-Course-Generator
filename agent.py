4e6a9d86bfe4c53c25565455e67df7868571af53
ATenorSaxophone <noah.benner@gmail.com>
2026-05-26 08:33:00 -0400

    Added code to get API key from .env file.

--- agent.py
+++ agent.py
@@ -1,4 +1,6 @@
 # Agent AI that creates Canvas Class about Human-Computer Interaction (HCI) using LangChain and Anthropic's Claude API. The agent will use the FastMCP server to create a Canvas Class with the specified parameters.
+import os
+
 from dotenv import load_dotenv
 
 from langchain.agents import create_agent
@@ -9,6 +11,7 @@ from mcp.server.fastmcp import FastMCP
 
 # Get .env variables
 load_dotenv()
+deepseek_key = os.getenv("MY_DEEPSEEK_KEY")
 
 @tool
 def create_canvas_module():
