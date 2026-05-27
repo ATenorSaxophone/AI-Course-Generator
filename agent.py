55a327d1268530272c9c3579a2660ded3094648d
ATenorSaxophone <noah.benner@gmail.com>
2026-05-27 09:22:43 -0400

    Connected workflow.md to agent.py to use as a system prompt. Fixed several bugs to allow agent.py to run.

--- agent.py
+++ agent.py
@@ -3,10 +3,10 @@ import os
 
 from dotenv import load_dotenv
 
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from deepagents import create_deep_agent
 from langchain.tools import tool
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.tools import DuckDuckGoSearchRun
 
 from mcp.server.fastmcp import FastMCP
 
@@ -15,46 +15,45 @@ load_dotenv()
 deepseek_key = os.getenv("MY_DEEPSEEK_KEY")
 
 #create DeepSeek model
-deepseek_model = OpenAI(
+deepseek_model = ChatOpenAI(
     model="deepseek-v4-flash",
-    open_api_base="https://api.deepseek.com",
-    open_api_key=deepseek_key,
+    base_url="https://api.deepseek.com",
+    api_key=os.environ["MY_DEEPSEEK_KEY"],
     temperature=0.7
 )
 
-@tool
-def create_canvas_module():
-    return 0
+# @tool
+# def create_canvas_module():
+#     return 0
 
-@tool
-def create_canvas_page():
-    return 0
+# @tool
+# def create_canvas_page():
+#     return 0
 
-@tool
-def create_canvas_quiz():
-    return 0
+# @tool
+# def create_canvas_quiz():
+#     return 0
 
-@tool
-def create_canvas_assignment():
-    return 0
+# @tool
+# def create_canvas_assignment():
+#     return 0
 
 #Create search tool using Tavily Search Results
-search_tool = TavilySearchResults(
-    max_results=5,
-    include_answer=True,
-    search_depth="advanced"
-)
+search_tool = DuckDuckGoSearchRun()
 
+#Read markdown file for system prompt
+with open("workflow.md", "r") as file:
+    system_prompt = file.read()
 
 #create agent with tools and deepseek model
 agent = create_deep_agent(
     model=deepseek_model,
-    tools=[
-        create_canvas_module,
-        create_canvas_page,
-        create_canvas_quiz,
-        create_canvas_assignment,
-        search_tool
-    ],
-    system_prompt=""
+    # tools=[
+    #     create_canvas_module,
+    #     create_canvas_page,
+    #     create_canvas_quiz,
+    #     create_canvas_assignment,
+    #     search_tool
+    # ],
+    system_prompt=system_prompt
 )
