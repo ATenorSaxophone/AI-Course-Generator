3c0d64d0716ec21af0a0dc6cd17618d587323ab1
ATenorSaxophone <noah.benner@gmail.com>
2026-05-30 11:54:54 -0400

    Created second agent for quizzes and assignments.

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 # Agent AI that creates Canvas Class about Human-Computer Interaction (HCI) using LangChain and Anthropic's Claude API. The agent will use the FastMCP server to create a Canvas Class with the specified parameters.
 import json
 import os
+import time
 
 from dotenv import load_dotenv
 
@@ -14,28 +15,35 @@ load_dotenv()
 google_key = os.getenv("MY_GOOGLE_KEY")
 
 #Create Google AI Studio model
-google_chat_model = ChatGoogleGenerativeAI(
+google_lesson_model = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
     api_key=os.environ["MY_GOOGLE_KEY"],
     temperature=0.7
 )
 
-google_video_model = ChatGoogleGenerativeAI(
-    model="gemini-3.5-flash",
+google_quiz_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
     api_key=os.environ["MY_GOOGLE_KEY"],
-    temperature=0.7,
+    temperature=0.7
 )
 
 #Create search tool using DuckDuckGoSearchRun from langchain_community
 search_tool = DuckDuckGoSearchRun()
 
 #Read markdown file for system prompt
-with open("workflow.md", "r", encoding="utf-8") as file:
+with open("system_prompts/lesson_workflow.md", "r", encoding="utf-8") as file:
     system_prompt = file.read()
 
-#Create google agent
-google_chat_agent = create_agent(
-    model=google_chat_model,   #Google Gemini model
+#Create google lesson agent
+google_lesson_agent = create_agent(
+    model=google_lesson_model,   #Google Gemini model
+    tools=[search_tool],    #Search tool for gathering dynamic information
+    system_prompt=system_prompt     #Instructions for the agent on how to create the Canvas Class and Canvas components.
+)
+
+#Create google quiz agent
+google_quiz_agent = create_agent(
+    model=google_quiz_model,   #Google Gemini model
     tools=[search_tool],    #Search tool for gathering dynamic information
     system_prompt=system_prompt     #Instructions for the agent on how to create the Canvas Class and Canvas components.
 )
@@ -56,16 +64,22 @@ for key, value in prompts.items():
 
     if key == "lesson 15":
         final_project_prompt = value["final presentation prompt"]
-        print(final_project_prompt)
     
-        final_project_output = google_chat_agent.invoke({"messages": [{"role": "user", "content": final_project_prompt}]})
+        final_project_output = google_lesson_agent.invoke({"messages": [{"role": "user", "content": final_project_prompt}]})
+        print("Final Project Finished!")
+        print("Waiting for 60 seconds before moving on to the next lesson to avoid hitting rate limits...")
+        time.sleep(60)  # Add a short delay between calls to avoid hitting rate limits
+
         print(f"Final Project Output for {key}:\n{final_project_output}\n")
 
     elif key == "lesson 16":
         final_exam_prompt = value["final exam prompt"]
-        print(final_exam_prompt)
     
-        final_exam_output = google_chat_agent.invoke({"messages": [{"role": "user", "content": final_exam_prompt}]})
+        final_exam_output = google_quiz_agent.invoke({"messages": [{"role": "user", "content": final_exam_prompt}]})
+        print("Final Exam Finished!")
+        print("Waiting for 60 seconds before moving on to the next lesson to avoid hitting rate limits...")
+        time.sleep(60)  # Add a short delay between calls to avoid hitting rate limits
+
         print(f"Final Exam Output for {key}:\n{final_exam_output}\n")
 
     else:
@@ -74,10 +88,21 @@ for key, value in prompts.items():
         assignment_prompt = value["assignment prompt"]
         
         # Run the agent with the combined prompt
-        lesson_output = google_chat_agent.invoke({"messages": [{"role": "user", "content": lesson_prompt}]})
-        quiz_output = google_chat_agent.invoke({"messages": [{"role": "user", "content": quiz_prompt}]})
-        assignment_output = google_chat_agent.invoke({"messages": [{"role": "user", "content": assignment_prompt}]})
-    
+        lesson_output = google_lesson_agent.invoke({"messages": [{"role": "user", "content": lesson_prompt}]})
+        print("Lesson Finished!")
+        print("Waiting for 60 seconds before invoking the quiz and assignment prompts to avoid hitting rate limits...")
+        time.sleep(60)  # Add a short delay between calls to avoid hitting rate limits
+
+        quiz_output = google_quiz_agent.invoke({"messages": [{"role": "user", "content": quiz_prompt}]})
+        print("Quiz Finished!")
+        print("Waiting for 60 seconds before invoking the assignment prompt to avoid hitting rate limits...")
+        time.sleep(60)  # Add a short delay between calls to avoid hitting rate limits
+
+        assignment_output = google_quiz_agent.invoke({"messages": [{"role": "user", "content": assignment_prompt}]})
+        print("Assignment Finished!")
+        print("Waiting for 60 seconds before moving on to the next lesson to avoid hitting rate limits...")
+        time.sleep(60)  # Add a short delay between calls to avoid hitting rate limits
+
         print(f"Readings Output for {key}:\n{lesson_output}\n")
         print(f"Quiz Output for {key}:\n{quiz_output}\n")
         print(f"Assignment Output for {key}:\n{assignment_output}\n")
